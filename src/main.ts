8902d03f01ce3aa3af9c9e8c11d5dbeb9447d15a
Ngô Đình Phước <ngodinhphuoc100@gmail.com>
2024-11-28 10:41:53 +0700

    modal view control

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // chạy trên cổng 3000
-
+  app.useStaticAssets(join(__dirname, '..', 'public')); // js,css , images
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
   await app.listen(3000);
 }
 bootstrap();
