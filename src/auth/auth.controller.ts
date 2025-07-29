010af7c85d257360af4d0625daecab7dd58719a8
ngodinhphuoc <ngodinhphuoc100@gmail.com>
2025-07-29 16:43:41 +0700

    api get account

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,33 +22,38 @@ import { Response } from 'express';
 @ApiTags(api_tags.AUTH)
 @ApiBearerAuth('access-token') // 👈 trùng với tên ở .addBearerAuth()
 @Controller('auth')
-@Public()
+
 export class AuthController {
   constructor(private readonly companiesService: AuthService) {}
   @response_Message("Register a new user")
+  @Public()
   @Post('/register')
   create(@Body() createCompanyDto: RegisterUserDto, @User() user: IUser) {
     return this.companiesService.register(createCompanyDto);
   }
-
+  @Public()
   @response_Message("User Login")
   @Post('/login')
   login(@Body() login: Login ,@Res({ passthrough: true }) response: Response) {
     return this.companiesService.login(login,response);
   }
 
-//   @response_Message("Lấy danh sách thành công")
-//   @Get()
-//   //   @ApiOperation({ summary: 'Lấy danh sách công ty có phân trang và lọc nâng cao' })
-//   // @ApiQuery({ name: 'page', required: false, example: 1, description: 'Trang hiện tại (bắt đầu từ 1)' })
-//   // @ApiQuery({ name: 'limit', required: false, example: 10, description: 'Số lượng mục trên mỗi trang' })
-//   // @ApiQuery({ name: 'name', required: false, example: 'ABC', description: 'Lọc theo tên công ty' })
-//   // @ApiQuery({ name: 'sort', required: false, example: '-createdAt', description: 'Sắp xếp' })
-//   // @ApiQuery({ name: 'populate', required: false, example: 'owner', description: 'Quan hệ cần populate' })
-//   findAll(@Query('page') currentPage : string , 
-//   @Query('limit') limit : string,
-//   @Query() qs : QueryCompany,
-// )  {
-//     return this.companiesService.findAll(+currentPage,+limit,qs);
-//   }
+  @response_Message("Get user information")
+  @Get('/acccount')
+  //   @ApiOperation({ summary: 'Lấy danh sách công ty có phân trang và lọc nâng cao' })
+  // @ApiQuery({ name: 'page', required: false, example: 1, description: 'Trang hiện tại (bắt đầu từ 1)' })
+  // @ApiQuery({ name: 'limit', required: false, example: 10, description: 'Số lượng mục trên mỗi trang' })
+  // @ApiQuery({ name: 'name', required: false, example: 'ABC', description: 'Lọc theo tên công ty' })
+  // @ApiQuery({ name: 'sort', required: false, example: '-createdAt', description: 'Sắp xếp' })
+  // @ApiQuery({ name: 'populate', required: false, example: 'owner', description: 'Quan hệ cần populate' })
+  findAll(@User() user : IUser
+  // req.user
+)  {
+  const { _id , name , email , role} = user
+    return {
+      user : {
+        _id , name , email , role
+      }
+    }
+  }
 }
